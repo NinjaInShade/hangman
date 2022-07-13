4313c08d922e4b91e0bc2b6c8f016fbed198f87c
Leon Michalak <leonmichalak6@gmail.com>
2022-07-13 09:47:21 +0100

    Lives displayed, alphabet displayed + keyboard handling added (no edge case stuff done yet)

--- hangman.js
+++ hangman.js
@@ -1,1 +1,27 @@
-console.log('test');
+const livesTxt = document.getElementById('lives');
+const alphabetContainer = document.getElementById('alphabet-container');
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+let lives = 10;
+
+const setupGame = () => {
+  livesTxt.innerText = `Lives: ${lives}`;
+
+  // Display alphabet
+  for (let i = 0; i < alphabet.length; i++) {
+    const newLetter = document.createElement('div');
+
+    newLetter.classList.add('letter');
+    newLetter.setAttribute('id', alphabet[i]);
+    newLetter.innerText = alphabet[i];
+
+    alphabetContainer.appendChild(newLetter);
+  }
+
+  // Add event listener for keyboard press
+  document.addEventListener('keydown', (e) => {
+    document.getElementById(e.key).classList.add('letter--pressed');
+  });
+};
+
+setupGame();
